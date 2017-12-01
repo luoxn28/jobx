445aba7b5faf751b1f3d32bf9920e85150ed0817
luoxn28 <luoxn28@163.com>
2017-12-01 22:16:08 +0800

    更新

---
+++ src/pages/Index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="index">
+    <header class="toolbar">
+      <el-menu id="menu-top" theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+        <el-button id="title" type="text">任务调度中心</el-button>
+      </el-menu>
+    </header>
+
+    <el-row class="tac">
+      <el-col :span="4">
+        <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" theme="dark">
+          <router-link to="/index/job">
+            <el-menu-item index="1">任务管理</el-menu-item>
+          </router-link>
+          <router-link to="/index/log">
+            <el-menu-item index="2">调度日志</el-menu-item>
+          </router-link>
+          <router-link to="/index/executor">
+            <el-menu-item index="3">执行器管理</el-menu-item>
+          </router-link>
+          <router-link to="/index/executor-group">
+            <el-menu-item index="4">任务组管理</el-menu-item>
+          </router-link>
+          <el-submenu index="5">
+            <template slot="title">导航</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+            <el-menu-item index="1-3">选项3</el-menu-item>
+            <el-menu-item index="1-4">选项4</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-col>
+
+      <el-col :span="20">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+    data () {
+      return {
+        activeIndex: '2'
+      }
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style>
+  #menu-top {
+    height: 50px;
+  }
+  #title {
+    font-size: large;
+    margin-left: 40px;
+    margin-top: 10px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,10 @@ import router from '../router'
 Vue.use(Router)
 
 import Error404 from '../components/Error404.vue'
-import HelloWorld from '../components/HelloWorld.vue'
+import Index from '../pages/Index.vue'
 
 var routes = [
-  {path: "/", name: 'HelloWorld', meta: {title: 'jobx测试平台', filter: false}, component:HelloWorld},
+  {path: "/", name: 'Index', meta: {title: 'jobx测试平台', filter: false}, component:Index},
   {path: '*', name: 'Error404', meta: {title: '404', filter: false}, component: Error404}
 ];
 
