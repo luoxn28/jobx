c753a33f7cbc33acca5b2b3f9c25c7698db30182
luoxn28 <luoxn28@163.com>
2017-12-01 18:30:29 +0800

    项目整理

---
+++ src/components/Error404.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="error404">
+    <p>
+      ███████╗██████╗ ██████╗  ██████╗ ██████╗     ██╗  ██╗ ██████╗ ██╗  ██╗<br/>
+      ██╔════╝██╔══██╗██╔══██╗██╔═══██╗██╔══██╗    ██║  ██║██╔═████╗██║  ██║<br/>
+      █████╗  ██████╔╝██████╔╝██║   ██║██████╔╝    ███████║██║██╔██║███████║<br/>
+      ██╔══╝  ██╔══██╗██╔══██╗██║   ██║██╔══██╗    ╚════██║████╔╝██║╚════██║<br/>
+      ███████╗██║  ██║██║  ██║╚██████╔╝██║  ██║         ██║╚██████╔╝     ██║<br/>
+      ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝         ╚═╝ ╚═════╝      ╚═╝<br/>
+    </p>
+    <h1>{{ msg }}</h1>
+    <h4>{{ notice }}</h4>
+  </div>
+</template>
+
+<script>
+  export default
+  {
+    name: 'Error404',
+    data () {
+      return {
+        msg:  '404页面',
+        notice: 'jobx -- A distributed task scheduling system.'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #error404 {
+    text-align: center;
+    color: brown;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import router from '../router'
 
 Vue.use(Router)
 
+import Error404 from '../components/Error404.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+
+var routes = [
+  {path: "/", name: 'HelloWorld', meta: {title: 'jobx测试平台', filter: false}, component:HelloWorld},
+  {path: '*', name: 'Error404', meta: {title: '404', filter: false}, component: Error404}
+];
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  mode: 'history',
+  routes: routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+
+  next();
+});
